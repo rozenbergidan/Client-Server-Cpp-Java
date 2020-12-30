0fd4394ac06b2e01551330c1df67cd5d040c9c3d
rozenbergidan <idanroze@post.bgu.ac.il>
2020-12-30 17:26:59 +0200

    added admin class

---
+++ spl-net/src/main/java/bgu/spl/net/impl/Database/Admin.java
@@ -0,0 +1,11 @@
+package bgu.spl.net.impl.Database;
+
+public class Admin {
+    public String username;
+    public String password;
+
+    public Admin(String _username, String _password){
+        username=_username;
+        password=_password;
+    }
+}
