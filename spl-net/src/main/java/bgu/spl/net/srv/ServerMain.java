4e886393190cb544750355d043fec9a174c56dc8
tsuri <tsurif@post.bgu.ac.il>
2021-01-06 17:08:23 +0200

    server main commit

--- spl-net/src/main/java/bgu/spl/net/srv/ServerMain.java
+++ spl-net/src/main/java/bgu/spl/net/srv/ServerMain.java
@@ -11,7 +11,7 @@ import java.util.Dictionary;
 public class ServerMain {
     public static void main(String[] args){
         try(Reactor<String> reactor = new Reactor<>(10,7777,()->new BGRSProtocol(),()->new BGRSEncoderDecoder())){
-            Database.getInstance().initialize("C:\\Users\\tsuri\\Desktop\\studies\\Year2\\SemesterA\\SystemProgramming\\Assingments\\Assignment3\\spl3\\Courses.txt");
+            Database.getInstance().initialize("../Courses.txt");
             reactor.serve();
         }catch (Exception e){}
     }
